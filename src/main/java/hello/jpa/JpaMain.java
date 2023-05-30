adb27db2c904cf120a40e180f32e9e2cf2881ab7
shinjh0224 <sshinn0224@gmail.com>
2023-05-30 17:01:41 +0900

    섹션3까지

--- src/main/java/hello/jpa/JpaMain.java
+++ src/main/java/hello/jpa/JpaMain.java
@@ -9,26 +9,30 @@ import java.util.List;
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
         EntityManager em = emf.createEntityManager();
+
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
         try {
-//            Member findMember = em.find(Member.class, 1L);
-//            findMember.setName("HelloJPA");
-            List<Member> resultList = em.createQuery("select m from Member as m", Member.class)
-                    .getResultList();
+            // code
+            Member member = em.find(Member.class, 5L);
+            System.out.println("=================");
 
-            for (Member member : resultList) {
-                System.out.println("member.getName() = " + member.getName());
-                
-            }
+            member.setName("ZZZZ");
+
+
+
             tx.commit();
-        } catch (Exception e) {
+        } catch(Exception e) {
             tx.rollback();
         } finally {
             em.close();
         }
+
         emf.close();
+
+
     }
 }
